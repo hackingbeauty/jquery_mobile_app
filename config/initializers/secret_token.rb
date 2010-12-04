02c1867731298c53fa27bbde5c668124dee83bf9
Mark Muskardin <markmuskardin@gmail.com>
2010-12-04 13:20:04 -0800

    Adding rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JqueryMobileApp::Application.config.secret_token = 'e1713d0484a05044e60de4ce99a28ad04c63a3119ec111d66dd38c5e0bf6abbb4a6494bffb724554435e2e93c158d1537c9659d6850115247b2871016b40d360'
